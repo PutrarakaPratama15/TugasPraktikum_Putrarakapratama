ff73c01775c547559d60242b89249ac3a8334443
putra <putraraka9921@gmail.com>
2023-10-13 22:45:38 +0700

    menambahakan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----------------------------------------------------------------" << endl <<endl;
+    cout << "Nama : Putra raka pratama" << endl <<endl;
+    cout << "Npm : 2310631170146" << endl <<endl;
+    cout << "Kelas : B-INFORMATIKA" << endl <<endl;
+    cout << "Cita-Cita : Belum ketemu" << endl <<endl;
+    cout << "Motivasi masuk informatika : Lanjutin jurusan smk" << endl <<endl;
+    cout << "----------------------------------------------------------------" << endl <<endl;
+    return 0;
+}
